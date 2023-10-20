f144508873d1d22c6ffaf980cbd91208907364da
Sandra Chege <chegemwihaki88@gmail.com>
2023-10-20 19:32:59 +0300

    checks on the email validation

--- dist/index.js
+++ dist/index.js
@@ -1,60 +1,45 @@
 "use strict";
-const form = document.querySelector("form");
-const card1 = document.querySelector(".card");
-const card2 = document.querySelector(".card2");
-const emailInput = document.getElementById("email");
-const dismissButton = card2.querySelector("button");
-const errorMessage = document.createElement("div");
-//validate emails
-function ValidateEmail() {
-    //trim removes whitespaces
-    const userEmail = emailInput.value.trim();
-    /**
-     * 1. the first statement checks if the email is empty and returms an error message
-     * 2. the else if checks checks if the email format is valid if email is not empty.
-     *    If the email is invalid it displays an error message
-     * 3. If email is valid, errormessage is cleared, and the classname to card1 and
-     *    remove classname in card2
-     */
-    if (userEmail === "") {
-        errorMessage.textContent = "Please provide your email address";
+class EmailValidator {
+    constructor() {
+        this.form = document.querySelector("form");
+        this.card1 = document.querySelector(".card");
+        this.card2 = document.querySelector(".card2");
+        this.emailInput = document.getElementById("email");
+        this.dismissButton = this.card2.querySelector("button");
+        this.errorMessage = document.createElement("div");
+        this.init();
     }
-    else if (!isEmailValid(userEmail)) {
-        errorMessage.textContent = "Please provide a valid email";
+    isEmailValid(email) {
+        const emailRegex = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/;
+        return emailRegex.test(email);
     }
-    else {
-        errorMessage.textContent = "";
-        const userEmailSpan = document.querySelector("#useremailaddress");
-        userEmailSpan.textContent = userEmail;
-        card1.classList.add("hide");
-        card2.classList.remove("hide");
+    validateEmail() {
+        const userEmail = this.emailInput.value.trim();
+        if (userEmail === "") {
+            this.errorMessage.textContent = "Please provide your email address";
+        }
+        else if (!this.isEmailValid(userEmail)) {
+            this.errorMessage.textContent = "Please provide a valid email";
+        }
+        else {
+            this.errorMessage.textContent = "";
+            const userEmailSpan = document.querySelector("#useremailaddress");
+            userEmailSpan.textContent = userEmail;
+            this.card1.classList.add("hide");
+            this.card2.classList.remove("hide");
+        }
+    }
+    init() {
+        this.form.addEventListener("submit", (e) => {
+            e.preventDefault();
+            this.validateEmail();
+        });
+        this.dismissButton.addEventListener("click", () => {
+            this.card2.classList.add("hide");
+            this.card1.classList.remove("hide");
+        });
+        this.form.appendChild(this.errorMessage);
     }
 }
-/**
- * @param email has a valid format.
- * @returns true if the email pattern matches the regular expression:emailRegex and false if it doesn't
- */
-function isEmailValid(email) {
-    const emailRegex = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/;
-    return emailRegex.test(email);
-}
-/**
- * prevents default form submission behaviour
- * calls validateEmail(); function
- */
-form.addEventListener("submit", (e) => {
-    e.preventDefault();
-    ValidateEmail();
-});
-/**
- * listens to a click ebvent on dismiss btn and removes
- * the className in card1 and add it to card2
- */
-dismissButton.addEventListener("click", () => {
-    card2.classList.add("hide");
-    card1.classList.remove("hide");
-});
-/**
- * appends the error div to the form
- */
-form.appendChild(errorMessage);
+// Instantiate the class to run the code
+new EmailValidator();
--- src/index.ts
+++ src/index.ts
@@ -1,60 +1,62 @@
-const form = document.querySelector("form") as HTMLFormElement;
-const card1 = document.querySelector(".card") as HTMLElement;
-const card2 = document.querySelector(".card2") as HTMLElement;
-const emailInput = document.getElementById("email") as HTMLInputElement;
-const dismissButton = card2.querySelector("button") as HTMLButtonElement;
-const errorMessage = document.createElement("div");
+class EmailValidator {
+  private form: HTMLFormElement;
+  private card1: HTMLElement;
+  private card2: HTMLElement;
+  private emailInput: HTMLInputElement;
+  private dismissButton: HTMLButtonElement;
+  private errorMessage: HTMLDivElement;
 
-//validate emails
-function ValidateEmail() {
-  //trim removes whitespaces
-    const userEmail = emailInput.value.trim();
-    /**
-     * 1. the first statement checks if the email is empty and returms an error message
-     * 2. the else if checks checks if the email format is valid if email is not empty.
-     *    If the email is invalid it displays an error message
-     * 3. If email is valid, errormessage is cleared, and the classname to card1 and 
-     *    remove classname in card2
-     */
-    if (userEmail === "") {
-        errorMessage.textContent = "Please provide your email address";
-    } else if (!isEmailValid(userEmail)) {
-        errorMessage.textContent = "Please provide a valid email";
-    } else {
-        errorMessage.textContent = "";
-        
-        const userEmailSpan = document.querySelector("#useremailaddress") as HTMLSpanElement;
-        userEmailSpan.textContent = userEmail;
-        card1.classList.add("hide");
-        card2.classList.remove("hide");
-    }
-}
-/**
- * @param email has a valid format.
- * @returns true if the email pattern matches the regular expression:emailRegex and false if it doesn't
- */
-function isEmailValid(email: string): boolean {
+  constructor() {
+    this.form = document.querySelector("form") as HTMLFormElement;
+    this.card1 = document.querySelector(".card") as HTMLElement;
+    this.card2 = document.querySelector(".card2") as HTMLElement;
+    this.emailInput = document.getElementById("email") as HTMLInputElement;
+    this.dismissButton = this.card2.querySelector(
+      "button"
+    ) as HTMLButtonElement;
+    this.errorMessage = document.createElement("div");
+
+    this.init();
+  }
+
+  private isEmailValid(email: string): boolean {
     const emailRegex = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/;
     return emailRegex.test(email);
+  }
+
+  private validateEmail() {
+    const userEmail = this.emailInput.value.trim();
+
+    if (userEmail === "") {
+      this.errorMessage.textContent = "Please provide your email address";
+    } else if (!this.isEmailValid(userEmail)) {
+      this.errorMessage.textContent = "Please provide a valid email";
+    } else {
+      this.errorMessage.textContent = "";
+
+      const userEmailSpan = document.querySelector(
+        "#useremailaddress"
+      ) as HTMLSpanElement;
+      userEmailSpan.textContent = userEmail;
+      this.card1.classList.add("hide");
+      this.card2.classList.remove("hide");
+    }
+  }
+
+  private init() {
+    this.form.addEventListener("submit", (e) => {
+      e.preventDefault();
+      this.validateEmail();
+    });
+
+    this.dismissButton.addEventListener("click", () => {
+      this.card2.classList.add("hide");
+      this.card1.classList.remove("hide");
+    });
+
+    this.form.appendChild(this.errorMessage);
+  }
 }
 
-/**
- * prevents default form submission behaviour
- * calls validateEmail(); function
- */
-form.addEventListener("submit", (e) => {
-    e.preventDefault();
-    ValidateEmail();
-})
-/**
- * listens to a click ebvent on dismiss btn and removes 
- * the className in card1 and add it to card2
- */
-dismissButton.addEventListener("click", () => {
-  card2.classList.add("hide");
-  card1.classList.remove("hide");
-});
-/**
- * appends the error div to the form
- */
-form.appendChild(errorMessage);
+// Instantiate the class to run the code
+new EmailValidator();
